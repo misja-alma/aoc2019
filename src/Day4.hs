8f4f238497002cebd12983f43db40f65e8fbb4bf
Misja Alma <misja.alma2@ing.com>
2019-12-04 10:23:29 +0100

    Day 4

---
+++ src/Day4.hs
@@ -0,0 +1,43 @@
+module Day4 where
+
+import Data.List
+
+twoSameDigits :: Int -> Bool
+twoSameDigits x = length (nub $ show x) < 6
+
+exactlyTwoSameDigits :: Int -> Bool
+exactlyTwoSameDigits x = search2Combi (show x) []
+
+search2Combi :: String -> String -> Bool
+search2Combi [] currentCombi = length currentCombi == 2
+search2Combi (c: cs) []      = search2Combi cs [c]
+search2Combi (c: cs) currentCombi
+  | c == head currentCombi   = search2Combi cs (c: currentCombi) 
+  | length currentCombi == 2 = True 
+  | otherwise                = search2Combi cs [c]
+
+within :: (Int, Int) -> Int -> Bool
+within (a, b) c = let (smaller, greater) = if a > b then (b, a) else (a, b) in
+                  c >= smaller && c <= greater
+
+increasingNumbers :: [Int]
+increasingNumbers = do
+  x1 <- [0..9]
+  x2 <- [x1..9]
+  x3 <- [x2..9]
+  x4 <- [x3..9]
+  x5 <- [x4..9]
+  x6 <- [x5..9]
+  return $ x6 + 10 * (x5 + 10 * (x4 + 10 * (x3 + 10 * (x2 + 10 * x1))))
+
+part1 :: IO()
+part1 = do
+  let inRange = filter (within (153517, 630395)) increasingNumbers
+  let solution = length $ filter twoSameDigits inRange
+  putStrLn $ "Solution: " ++ show solution
+
+part2 :: IO()
+part2 = do
+   let inRange = filter (within (153517, 630395)) increasingNumbers
+   let solution = length $ filter exactlyTwoSameDigits inRange
+   putStrLn $ "Solution: " ++ show solution
