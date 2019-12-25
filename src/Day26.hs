664b89fe5b41ba6dec03064f0f0c9f519f1bad07
Misja Alma <misja.alma2@ing.com>
2019-12-25 10:22:26 +0100

    Day 26, solved manually

---
+++ src/Day26.hs
@@ -0,0 +1,168 @@
+module Day26 where
+
+import Data.List.Split
+import Data.Array
+import Data.List
+import Data.Function
+import Data.Char
+import Utils
+import Data.Maybe
+import Debug.Trace
+import qualified Data.Sequence as S
+import Data.Foldable
+import Data.Either
+import Control.Monad.Extra
+
+type Index = Int
+type Base = Index
+type Program = (Index, Base, Array Index Integer)
+type Input = [Integer]
+type Output = Integer
+data ProgramState = Running Program Input | WaitForOutput Program Input Output | Halted | WaitForInput Program
+type Operation = Input -> Program -> ProgramState
+type Opcode = Integer
+
+peek :: Array Index Integer -> Index -> Base -> Integer
+peek ar pt base = ar ! (fromInteger (ar ! pt) + base)
+
+getMode :: Opcode -> Int -> Integer
+getMode mode offset = case offset of
+    1 -> (mode `div` 100) `mod` 10
+    2 -> (mode `div` 1000) `mod` 10
+    3 -> (mode `div` 10000) `mod` 10
+
+readParam :: Program -> Index -> Opcode -> Integer
+readParam (ip, base, ar) offset mode = case getMode mode offset of
+    0 -> peek ar (ip + offset) 0
+    1 -> ar ! (ip + offset)
+    2 -> peek ar (ip + offset) base
+
+write :: Program -> Index -> Opcode -> Integer -> Array Index Integer
+write (ip, base, ar) offset mode value = case getMode mode offset of
+    2 -> ar // [(base + fromInteger (ar ! (ip + offset)), value)]
+    _ -> ar // [(fromInteger $ ar ! (ip + offset), value)]
+
+exit :: Operation
+exit _ _ = Halted
+
+add :: Opcode -> Operation
+add opcode inp prog@(ip, base, ar) =
+    let p1 = readParam prog 1 opcode
+        p2 = readParam prog 2 opcode
+        sum = p1 + p2
+        newAr = write prog 3 opcode sum in
+    Running (ip + 4, base, newAr) inp
+
+mul :: Opcode -> Operation
+mul opcode inp prog@(ip, base, ar) =
+    let p1 = readParam prog 1 opcode
+        p2 = readParam prog 2 opcode
+        product = p1 * p2
+        newAr = write prog 3 opcode product in
+    Running (ip + 4, base, newAr) inp
+
+input :: Opcode -> Operation
+input opcode inp prog@(ip, base, ar) =
+    if null inp then WaitForInput prog
+    else
+      let newAr = write prog 1 opcode (head inp) in
+      Running (ip + 2, base, newAr) (tail inp)
+
+output :: Opcode -> Operation
+output opcode inp prog@(ip, base, ar) =
+   let p1 = readParam prog 1 opcode in
+   WaitForOutput (ip + 2, base, ar) inp p1
+
+jumpIfTrue :: Opcode -> Operation
+jumpIfTrue opcode inp prog@(ip, base, ar) =
+      let p1 = readParam prog 1 opcode
+          p2 = readParam prog 2 opcode in
+      if p1 /= 0 then Running (fromInteger p2, base, ar) inp else Running (ip + 3, base, ar) inp
+
+jumpIfFalse :: Opcode -> Operation
+jumpIfFalse opcode inp prog@(ip, base, ar) =
+      let p1 = readParam prog 1 opcode
+          p2 = readParam prog 2 opcode in
+      if p1 == 0 then Running (fromInteger p2, base, ar) inp else Running (ip + 3, base, ar) inp
+
+lessThan :: Opcode -> Operation
+lessThan opcode inp prog@(ip, base, ar) =
+      let p1 = readParam prog 1 opcode
+          p2 = readParam prog 2 opcode
+          newAr = if p1 < p2 then write prog 3 opcode 1 else write prog 3 opcode 0 in
+      Running (ip + 4, base, newAr) inp
+
+equals :: Opcode -> Operation
+equals opcode inp prog@(ip, base, ar) =
+      let p1 = readParam prog 1 opcode
+          p2 = readParam prog 2 opcode
+          newAr = if p1 == p2 then write prog 3 opcode 1 else write prog 3 opcode 0 in
+      Running (ip + 4, base, newAr) inp
+
+setBase :: Opcode -> Operation
+setBase opcode inp prog@(ip, base, ar) =
+    let p1 = readParam prog 1 opcode in
+    Running (ip + 2, base + fromInteger p1, ar) inp
+
+translate :: Opcode -> Operation
+translate opcode =
+    case opcode `mod` 100 of
+      99 -> exit
+      1  -> add opcode
+      2  -> mul opcode
+      3  -> input opcode
+      4  -> output opcode
+      5  -> jumpIfTrue opcode
+      6  -> jumpIfFalse opcode
+      7  -> lessThan opcode
+      8  -> equals opcode
+      9  -> setBase opcode
+
+execute :: Input -> Program -> ProgramState
+execute input prog@(pc, base, ar) =
+    let instr = ar ! pc
+        operation = translate instr
+        result = operation input prog in
+    case result of
+        Running newProg newInput -> execute newInput newProg
+        outputOrHalted           -> outputOrHalted
+
+collectOutputs :: Input -> Program -> (Program, [Output])
+collectOutputs input program = case execute input program of
+    Halted                        -> (program, [])
+    WaitForInput prog             -> (prog, [])
+    WaitForOutput prog inp outp   -> let (newProg, outputs) = collectOutputs inp prog in (newProg, outp : outputs)
+
+-- address, x, y
+data Packet = Packet Int Integer Integer deriving Show
+
+type InputQueue = S.Seq (Integer, Integer)
+
+commandsToInput :: [String] -> [Integer]
+commandsToInput cs = cs >>= (\s -> fromIntegral <$> (fmap ord s ++ [10]))
+
+getLines :: Input -> Program -> (Program, [String])
+getLines input prog = let (newProg, outputs) = collectOutputs input prog
+                      in (newProg, lines $ fmap (chr . fromInteger) outputs)
+
+toInput :: String -> Input
+toInput s = (fmap (fromIntegral . ord) s) ++ [10]
+
+readAndEval :: Program -> IO Program
+readAndEval prog = do
+     cl <- getLine
+     let (prog2, outputs) = getLines (toInput cl) prog
+     putStrLn $ unlines outputs
+     return prog2
+
+part1 :: IO ()
+part1 = do
+    content <- readFile "resources/day26.txt"
+    let numbers = (read :: String -> Integer) <$> splitOn "," content
+    let buffer = replicate 1000 0
+    let opArray = array (0, length numbers + length buffer  - 1) $ zip [0..] (numbers ++ buffer)
+    let program = (0,0,opArray)
+    let (prog, outputs) = getLines [] program
+    putStrLn $ unlines outputs
+    iterateM readAndEval prog
+    return ()
